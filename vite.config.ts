f2d4891517c7ec98b1ca2ba443918f65061451b5
Budimir Budimir <3019206+budimirbudimir@users.noreply.github.com>
2022-02-17 02:11:25 +0100

    update for ghpages 2

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,6 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 export default defineConfig({
   plugins: [reactRefresh(), tsconfigPaths()],
   build: {
-    outDir: 'docs',
+    outDir: 'dist',
   },
 });
