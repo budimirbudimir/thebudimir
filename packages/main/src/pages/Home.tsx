3c62e0b16ededccc3011efa216f9829254429519
Budimir Budimir <bb@p200300eac718fa47bb5ae6044538b124.dip0.t-ipconnect.de>
2026-01-29 04:07:59 +0100

    fix: update Home page UI

--- packages/main/src/pages/Home.tsx
+++ packages/main/src/pages/Home.tsx
@@ -3,48 +3,22 @@ import '../App.css';
 export default function Home() {
   return (
     <div className="App">
-      <p className="Title">Budimir Budimir</p>
+      <h1>Budimir Budimir</h1>
+      <p>Crafting software since 2001</p>
+      <p className="Principles">
+        <span className="Transparency">Transparency.</span> <span className="Empathy">Empathy.</span> <span className="Purpose">Purpose.</span>
+      </p>
+      <p>If you have those, send me the message, I promise I will give my best to read it and reply.</p>
 
-      <p className="Quote">My four core principles:</p>
-      <ul className="List">
-        <li>Strong opinions, loosely held</li>
-        <li>First do it, then do it right, then do it better</li>
-        <li>Do not ship broken windows</li>
-        <li>Never stop learning</li>
-      </ul>
-
-      <div className="Icons">
-        <a
-          target="_blank"
-          href="https://www.linkedin.com/in/budimirbudimir/"
-          title="LinkedIn"
-          rel="noopener"
-        >
-          <img
-            className="Icon"
-            height="32"
-            width="32"
-            src="https://unpkg.com/simple-icons@v6/icons/linkedin.svg"
-            alt="LinkedIn"
-          />
+      <div className="Social">
+        <a target="_blank" href="https://www.linkedin.com/in/budimirbudimir" title="LinkedIn" rel="noreferrer">
+          <img className="Icon" alt="LinkedIn" height="32" width="32" src="https://unpkg.com/simple-icons@v6/icons/linkedin.svg" />
         </a>
-        <a target="_blank" href="https://github.com/budimirbudimir" title="GitHub" rel="noopener">
-          <img
-            className="Icon"
-            height="32"
-            width="32"
-            src="https://unpkg.com/simple-icons@v6/icons/github.svg"
-            alt="GitHub"
-          />
+        <a target="_blank" href="https://github.com/budimirbudimir" title="GitHub" rel="noreferrer">
+          <img className="Icon" alt="GitHub" height="32" width="32" src="https://unpkg.com/simple-icons@v6/icons/github.svg" />
         </a>
-        <a target="_blank" href="mailto:budimirbudimir@msn.com" title="Email" rel="noopener">
-          <img
-            className="Icon"
-            height="32"
-            width="32"
-            src="https://unpkg.com/simple-icons@v6/icons/microsoftoutlook.svg"
-            alt="Email"
-          />
+        <a target="_blank" href="mailto:budimirbudimir@msn.com" title="Email" rel="noreferrer">
+          <img className="Icon" alt="Email" height="32" width="32" src="https://unpkg.com/simple-icons@v6/icons/microsoftoutlook.svg" />
         </a>
       </div>
     </div>
