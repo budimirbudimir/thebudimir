1bedadeef4a3521b314272330e6cd04517eca5d7
Budimir Budimir <3019206+budimirbudimir@users.noreply.github.com>
2026-02-19 23:43:35 +0100

    fix: build file paths (#65)
    * feat: fix convo persistence and add patch for router
    
    * fix: build files
    
    * fix: build file paths

--- packages/main/vite.config.ts
+++ packages/main/vite.config.ts
@@ -1,11 +1,11 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
-import { ghPages } from 'vite-plugin-gh-pages';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths(), ghPages()],
+  plugins: [react(), tsconfigPaths()],
+  base: '/',
   build: {
     outDir: '../../dist/main',
     assetsDir: 'assets',
