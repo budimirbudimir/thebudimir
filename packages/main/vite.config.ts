ae6d08c6eb8fce5a150ab7d8f62ca028f002cead
Budimir Budimir <budimirbudimir@msn.com>
2026-02-19 23:24:50 +0100

    fix: build files

--- packages/main/vite.config.ts
+++ packages/main/vite.config.ts
@@ -6,7 +6,9 @@ import { ghPages } from 'vite-plugin-gh-pages';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths(), ghPages()],
+  base: '/',
   build: {
     outDir: '../../dist/main',
+    assetsDir: 'assets',
   },
 });
